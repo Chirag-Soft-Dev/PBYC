34a752e6b42df438d929641f2e022f3f66d606cb
sdac-7 <sdac-7@sdac-7-PC>
2024-11-25 11:58:22 +0530

    consumer

---
+++ pbyc/src/account/Account.java
@@ -0,0 +1,14 @@
+package account;
+
+public class Account extends company.Company {
+	@Override
+	public double calsal(double sal) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public double calsal(double sal, double bonus) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ pbyc/src/manager/Manager.java
@@ -0,0 +1,9 @@
+package manager;
+
+public class Manager extends account.Account{
+	@Override
+	public double calsal(double sal, double bonus) {
+		// TODO Auto-generated method stub
+		return sal+bonus;
+	}
+}
